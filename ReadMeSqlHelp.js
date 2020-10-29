50af56f7db951b17ea82c8fa620a353608348a58
Shriyash Parandkar <41060734+ganeshparandkar@users.noreply.github.com>
2020-10-30 03:31:10 +0530

    database fetching completed

---
+++ ReadMeSqlHelp.js
@@ -0,0 +1,32 @@
+// *  to open shell
+// python manage.py shell
+
+// * import statements
+// from blog.models import Post
+// from django.contrib.auth.models import User
+
+// * to get the queryset (all users)
+// User.objects.all()
+
+// * to get the queryset (1st user same for last user)
+// User.objects.first()
+
+// * to filter user
+// User.objects.filter(username = 'Shriyash')
+
+
+// user =  User.objects.filter(username = 'Shriyash')
+
+// user.id 
+// user.name
+// ?-------------------------------
+
+// Post.objects.all()
+// * to create a post (type all in one line )
+// post1 = Post(
+//     title = 'Blog1',
+//     content = 'first one',
+//     author = user 
+// )
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
+from .models import *
 
-posts = [
-    {
-        'author': 'Shriyash',
-        'title': 'Django Framework',
-        'content': 'My Django Project List ',
-        'date': 'October 30 2020'
-    },
-    {
-        'author': 'Ritika',
-        'title': 'php Framework',
-        'content': 'My Php Project List ',
-        'date': 'March 30 2020'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Shriyash',
+#         'title': 'Django Framework',
+#         'content': 'My Django Project List ',
+#         'datePosted': 'January 30, 2020'
+#     },
+#     {
+#         'author': 'Ritika',
+#         'title': 'php Framework',
+#         'content': 'My Php Project List ',
+#         'datePosted': 'March 30, 2020'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        # ? Post is your model this data is directly gonna come from your database
+        # 'posts' : posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
